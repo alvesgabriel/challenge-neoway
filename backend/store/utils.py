a74acad38e2fa9c9744687991595b8f337249633
Gabriel Alves <gabriel.alves.monteiro1@gmail.com>
2021-08-19 19:46:48 -0300

    :sparkles: Created data importation to data base
    close #5

---
+++ backend/store/utils.py
@@ -0,0 +1,78 @@
+import functools
+import re
+
+import validate_docbr as docbr
+
+
+def parse_file(lines, file):
+    buyer_doc = slice(0, 19)
+    private = slice(19, 31)
+    incomplete = slice(31, 43)
+    created_at = slice(43, 65)
+    ticket_average = slice(65, 87)
+    ticket = slice(87, 111)
+    seller_doc_most_frequency = slice(111, 131)
+    seller_doc = slice(131, 152)
+
+    return [
+        {
+            "file": file,
+            "buyer_doc": digits(line[buyer_doc]),
+            "private": to_bool(line[private]),
+            "incomplete": to_bool(line[incomplete]),
+            "created_at": to_date(line[created_at]),
+            "ticket_average": to_float(line[ticket_average]),
+            "ticket": to_float(line[ticket]),
+            "seller_doc_most_frequency": digits(
+                line[seller_doc_most_frequency]
+            ),
+            "seller_doc": digits(line[seller_doc]),
+            "invalid_buyer_doc": validate_document(line[buyer_doc]),
+            "invalid_seller_doc": validate_document(line[seller_doc]),
+            "invalid_seller_doc_most_frequency": validate_document(
+                line[seller_doc_most_frequency]
+            ),
+        }
+        for line in lines
+        if not line.lower().startswith(b"cpf")
+    ]
+
+
+def to_str(function):
+    @functools.wraps(function)
+    def wrapper(value):
+        value = value.strip().decode("utf-8")
+        if value == "NULL":
+            return function("")
+        return function(value)
+
+    return wrapper
+
+
+@to_str
+def digits(value):
+    return re.sub("\D", "", value) or None
+
+
+@to_str
+def to_float(value):
+    return re.sub(",", ".", value) or None
+
+
+@to_str
+def to_bool(value):
+    return bool(int(value))
+
+
+@to_str
+def to_date(value):
+    return value or None
+
+
+@to_str
+def validate_document(value):
+    docs = (
+        (docbr.CPF, value),
+        (docbr.CNPJ, value),
+    )
+    return not any(docbr.validate_docs(docs))
--- backend/store/views.py
+++ backend/store/views.py
@@ -1,17 +1,18 @@
-from datetime import datetime
+import uuid
 
-from django.db.models import query
 from rest_framework import generics, views
 from rest_framework.parsers import FileUploadParser
 from rest_framework.response import Response
 
-from .models import File
-from .serializers import FileSerializer
+from .models import File, Sale
+from .serializers import FileSerializer, SaleSerializer
+from .utils import parse_file
 
 
 class FileListView(generics.ListAPIView):
     queryset = File.objects.all()
     serializer_class = FileSerializer
+
 
 class FileDetailView(generics.RetrieveAPIView):
     queryset = File.objects.all()
@@ -24,11 +25,25 @@ class FileUploadView(views.APIView):
     parser_classes = [FileUploadParser]
 
     def put(self, request, filename, format=None, *args, **kwargs):
-        file_obj = request.data['file']
-        file_obj.name = f'{datetime.now()}-{filename}'
+        file_obj = request.data["file"]
+        file_obj.name = f"{uuid.uuid4().hex}.txt"
+        lines = file_obj.readlines()
 
         file = File(filename=file_obj)
         file.save()
 
+        sales = [Sale(**sale) for sale in parse_file(lines, file)]
+        Sale.objects.bulk_create(sales)
+
         serializer = FileSerializer(file)
         return Response(serializer.data, status=201)
+
+
+class SaleListView(generics.ListAPIView):
+    queryset = Sale.objects.all()
+    serializer_class = SaleSerializer
+
+
+class SaleDetailView(generics.RetrieveAPIView):
+    queryset = Sale.objects.all()
+    serializer_class = SaleSerializer
